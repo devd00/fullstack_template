b53de23b83c5967a0ed0e870990c97b4da72c3b3
Joydip Kumar <joydipkn@gmail.com>
2022-07-10 10:48:05 +0530

    added port and mongo connections

--- server.js
+++ server.js
@@ -8,12 +8,15 @@ require('dotenv').config()
 //point 4 from ins.txt
 let db,
     dbConnectionString = process.env.DB_STRING, //this pulls the connectionstring from the env file
-    dbName = 'souls-quotes', //from the mongoDB name
+    dbName = 'sample_dbase', //from the mongoDB name
     collection
 
 MongoClient.connect(dbConnectionString) // point 5 from ins.txt
     .then(client => {
         console.log('connected to database')
         db = client.db(dbName) //from the mongoDB name
-        collection = client.collection('quotes')
+        collection = db.collection('collection_z')
     })
+app.listen(process.env.PORT || PORT, () => {
+    console.log(`server is running on ${process.env.PORT}`)
+})
