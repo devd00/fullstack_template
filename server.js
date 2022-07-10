296d7f5256e7bc380b2c56ee4c0162ac4bc7ef40
Joydip Kumar <joydipkn@gmail.com>
2022-07-10 09:32:32 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,8 @@
+
+//point 3 from instructions.txt
+const express = require('express')
+const app = express()
+const cors = require('cors')
+const MongoClient = require('mongodb').MongoClient
+require('dotenv').config()
+
