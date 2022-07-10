3fa5f701c2974ce799ffcaf77a49ad909d0ae22c
Joydip Kumar <joydipkn@gmail.com>
2022-07-10 10:18:43 +0530

    added monogoDB connections

--- server.js
+++ server.js
@@ -5,4 +5,15 @@ const app = express()
 const cors = require('cors')
 const MongoClient = require('mongodb').MongoClient
 require('dotenv').config()
+//point 4 from ins.txt
+let db,
+    dbConnectionString = process.env.DB_STRING, //this pulls the connectionstring from the env file
+    dbName = 'souls-quotes', //from the mongoDB name
+    collection
 
+MongoClient.connect(dbConnectionString) // point 5 from ins.txt
+    .then(client => {
+        console.log('connected to database')
+        db = client.db(dbName) //from the mongoDB name
+        collection = client.collection('quotes')
+    })
